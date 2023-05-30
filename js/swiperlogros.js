55385f52abfcd2a3445a0603c6ef9f83fa884f81
Gustavo Castillo <gusgcast@espol.edu.ec>
2023-05-30 18:51:46 -0400

    modificacion cards perfil

--- js/swiperlogros.js
+++ js/swiperlogros.js
@@ -13,3 +13,24 @@ var swiperEl = document.querySelector(".mySwiper-lograras");
       },
     });
     swiperEl.initialize()
+    // SWIPE PERFIL
+    var swiper6El = document.querySelector(".mySwiper-cards");
+    Object.assign(swiper6El, {
+      grabCursor: true,
+      effect: "creative",
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          origin: "left center",
+          translate: ["-5%", 0, -200],
+          rotate: [0, 100, 0],
+        },
+        next: {
+          origin: "right center",
+          translate: ["5%", 0, -200],
+          rotate: [0, -100, 0],
+        },
+      },
+    });
+    swiper6El.initialize()
+    
