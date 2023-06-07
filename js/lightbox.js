542395b63f6d4ae5172e8dfc697b257936ba22f5
Gustavo Castillo <gusgcast@espol.edu.ec>
2023-06-06 20:38:29 -0400

    clic derecho disabled

--- js/lightbox.js
+++ js/lightbox.js
@@ -26,3 +26,24 @@ const aparecerImagen = (imagen)=>{
     imagenesLight.classList.toggle('showImage');
     icon_menu1.style.opacity = '0';
 }
+function disableIE() {
+    if (document.all) {
+        return false;
+    }
+}
+function disableNS(e) {
+    if (document.layers || (document.getElementById && !document.all)) {
+        if (e.which==2 || e.which==3) {
+            return false;
+        }
+    }
+}
+if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown = disableNS;
+} 
+else {
+    document.onmouseup = disableNS;
+    document.oncontextmenu = disableIE;
+}
+document.oncontextmenu=new Function("return false");
