1b40a46da3cb069effda1b4dad5f07803a2c8ff5
Gustavo Castillo <gusgcast@espol.edu.ec>
2023-06-14 11:57:03 -0400

    algunas modificaciones

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,13 +1,16 @@
+// lightbox galeria start
 const imagenes  = document.querySelectorAll('.img-galeria');
 const imagenesLight = document.querySelector('.agregar-imagen');
 const contenedorLight = document.querySelector('.imagen-light');
 const icon_menu1 = document.querySelector('.icon-menu');
-// console.log(imagenes);
-// console.log(imagenesLight);
-// console.log(contenedorLight);
+const swipercardperfil = document.querySelectorAll('#contenedor-swiper')
+
 imagenes.forEach(imagen =>{
    imagen.addEventListener('click', ()=>{
     aparecerImagen(imagen.getAttribute('src'));
+    swipercardperfil.forEach(element => {
+        element.style.opacity = '.6'
+    });
    })
 }
 
@@ -17,6 +20,9 @@ contenedorLight.addEventListener('click', (e)=>{
         contenedorLight.classList.toggle('show');
         imagenesLight.classList.toggle('showImage');
         icon_menu1.style.opacity = '1';
+        swipercardperfil.forEach(element => {
+            element.style.opacity = '1'
+        });
     }
 })
 
@@ -26,24 +32,92 @@ const aparecerImagen = (imagen)=>{
     imagenesLight.classList.toggle('showImage');
     icon_menu1.style.opacity = '0';
 }
-function disableIE() {
-    if (document.all) {
-        return false;
+// lighbox galeria end
+// 
+// disable right clic
+// function disableIE() {
+//     if (document.all) {
+//         return false;
+//     }
+// }
+// function disableNS(e) {
+//     if (document.layers || (document.getElementById && !document.all)) {
+//         if (e.which==2 || e.which==3) {
+//             return false;
+//         }
+//     }
+// }
+// if (document.layers) {
+//     document.captureEvents(Event.MOUSEDOWN);
+//     document.onmousedown = disableNS;
+// } 
+// else {
+//     document.onmouseup = disableNS;
+//     document.oncontextmenu = disableIE;
+// }
+// document.oncontextmenu=new Function("return false");
+
+
+// progress bar
+// spanprogress.forEach((span)=>{
+//     span.style.width = span.dataset.width;
+//     span.innerHTML = span.dataset.width;
+// })
+
+// TOTAL SLIDER MESSAGE START
+function contarSlider(){
+    let perfil = document.getElementsByClassName("slide-cards");
+    let logro = document.getElementsByClassName("slide-logro");
+    console.log(perfil);
+    console.log("logros=",logro);
+    for(let i=0 ; i<perfil.length;i++){
+        let elementohtml = `<div class="numberslider"> <p> ${i+1}/${perfil.length} </p> </div>`;
+        perfil[i].innerHTML += elementohtml;
     }
-}
-function disableNS(e) {
-    if (document.layers || (document.getElementById && !document.all)) {
-        if (e.which==2 || e.which==3) {
-            return false;
-        }
+    for(let i=0 ; i<perfil.length;i++){
+        let elementohtml = `<div class="numberslider"> <p> ${i+1}/${logro.length} </p> </div>`;
+        logro[i].innerHTML += elementohtml;
     }
+    
 }
-if (document.layers) {
-    document.captureEvents(Event.MOUSEDOWN);
-    document.onmousedown = disableNS;
-} 
-else {
-    document.onmouseup = disableNS;
-    document.oncontextmenu = disableIE;
+contarSlider();
+// TOTAL SLIDER MESSAGE END
+
+// PROGRESS HORIZONTAL BAR START
+const spanprogress = document.querySelectorAll('.progress-bar span');
+const contenedorbar = document.getElementById('progress-bar-box-test');
+const spandata = document.getElementById('spandata');
+
+function mostrarprogressbar(){
+  let scrollTop = document.documentElement.scrollTop;
+  let alturaAnimado = contenedorbar.offsetTop;
+  if(alturaAnimado+150<scrollTop){
+    spanprogress.forEach((span)=>{
+    span.style.width = span.dataset.width;
+    span.innerHTML = span.dataset.width;
+    })
+    }
+    
 }
-document.oncontextmenu=new Function("return false");
+
+window.addEventListener('scroll',mostrarprogressbar);
+function aumentadata(porentaje){
+    spandata.dataset.width=`${porentaje}% `;
+}
+var porcentajevalor =73;
+aumentadata(porcentajevalor);
+var temporizador = setInterval(function(){
+    let scrollTop = document.documentElement.scrollTop;
+    let alturaAnimado = contenedorbar.offsetTop;
+    let valorscroll = scrollTop-alturaAnimado;
+    console.log(valorscroll);
+    if(porcentajevalor<92 && (0<valorscroll<500)){
+        aumentadata(porcentajevalor);
+        mostrarprogressbar();
+        porcentajevalor++;
+    }
+},9000);
+
+// PROGRESS HORIZONTAL BAR END
+
+
